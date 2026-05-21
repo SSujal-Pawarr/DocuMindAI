770f09df14d7360c9602d5032fae39e244351798
SujalPawar <pawarsujal4455@gmail.com>
2026-05-21 14:39:49 +0530

    updated file

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from pymongo import MongoClient
 import bcrypt
 import pdfplumber
 import requests
+
+client = MongoClient("mongodb://localhost:27017/")
+d=client["ai_app"]
+users=db["users"]
+docs=db["documents"]
