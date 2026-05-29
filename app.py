7a5dc14a71c3a45ed385255af617d2e29bcad98e
SujalPawar <pawarsujal4455@gmail.com>
2026-05-29 22:35:54 +0530

    updated file

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def chat_page():
     if file:
         text = ""
         with pdfplumber.open(file) as pdf:
-            
+               for page in pdf.pages:
+                    text += page.extract_text() or ""    
